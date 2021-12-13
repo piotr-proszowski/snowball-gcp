898f20ceb32dbed4f691eff8d310ead96ed1df70
piotr-proszowski <piotr.proszowski@allegro.pl>
2021-12-13 13:13:34 +0100

    Save arena updates to big query

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,10 +1,18 @@
 package hello;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.bigquery.storage.v1.*;
+import com.google.protobuf.Descriptors;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,6 +21,58 @@ import java.util.stream.Stream;
 @SpringBootApplication
 @RestController
 public class Application {
+
+    static class WriteCommittedStream {
+
+        final JsonStreamWriter jsonStreamWriter;
+
+        public WriteCommittedStream(String projectId, String datasetName, String tableName) throws IOException, Descriptors.DescriptorValidationException, InterruptedException {
+
+            try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
+
+                WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.COMMITTED).build();
+                TableName parentTable = TableName.of(projectId, datasetName, tableName);
+                CreateWriteStreamRequest createWriteStreamRequest =
+                        CreateWriteStreamRequest.newBuilder()
+                                .setParent(parentTable.toString())
+                                .setWriteStream(stream)
+                                .build();
+
+                WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+                jsonStreamWriter = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
+            }
+        }
+
+        public ApiFuture<AppendRowsResponse> send(Arena arena) {
+            Instant now = Instant.now();
+            JSONArray jsonArray = new JSONArray();
+
+            arena.state.forEach((url, playerState) -> {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("x", playerState.x);
+                jsonObject.put("y", playerState.y);
+                jsonObject.put("direction", playerState.direction);
+                jsonObject.put("wasHit", playerState.wasHit);
+                jsonObject.put("score", playerState.score);
+                jsonObject.put("player", url);
+                jsonObject.put("timestamp", now.getEpochSecond() * 1000 * 1000);
+                jsonArray.put(jsonObject);
+            });
+
+            return jsonStreamWriter.append(jsonArray);
+        }
+
+    }
+
+    private final WriteCommittedStream writeCommittedStream;
+
+    public Application() throws Descriptors.DescriptorValidationException, IOException, InterruptedException {
+        final String projectId = ServiceOptions.getDefaultProjectId();
+        final String datasetName = "snowball";
+        final String tableName = "events";
+        writeCommittedStream = new WriteCommittedStream(projectId, datasetName, tableName);
+    }
 
     private static String MYSELF = "https://34.149.220.180.sslip.io/";
     private static int THROW_DISTANCE = 3;
@@ -91,6 +151,7 @@ public class Application {
 
     @PostMapping("/**")
     public String index(@RequestBody ArenaUpdate arenaUpdate) {
+        writeCommittedStream.send(arenaUpdate.arena);
         Map<String, PlayerState> statesOfPlayers = arenaUpdate.arena.state;
         PlayerState myState = statesOfPlayers.get(MYSELF);
 
@@ -104,9 +165,9 @@ public class Application {
             return "T";
         } else {
             List<PlayerState> playersStates = playersStatesStream.collect(Collectors.toList());
-            if (thereIsSomeoneOnMyRight(playersStates, myState)) {
+            if (thereIsSomeoneOnMyRight(playersStates.stream(), myState)) {
                 return "R";
-            } else if (thereIsSomeoneOnMyLeft(playersStates, myState)) {
+            } else if (thereIsSomeoneOnMyLeft(playersStates.stream(), myState)) {
                 return "L";
             } else if (iCanGoStraight(myState, arenaUpdate)) {
                 return "F";
@@ -124,7 +185,7 @@ public class Application {
         return afterIGoStraight.x < x && afterIGoStraight.y < y;
     }
 
-    private boolean thereIsSomeoneOnMyRight(List<PlayerState> players, PlayerState myState) {
+    private boolean thereIsSomeoneOnMyRight(Stream<PlayerState> players, PlayerState myState) {
         switch (myState.direction) {
             case "N":
                 return thereIsSomebodyOnMyWay(players, myState.withDirection("E"));
@@ -139,7 +200,7 @@ public class Application {
         }
     }
 
-    private boolean thereIsSomeoneOnMyLeft(List<PlayerState> players, PlayerState myState) {
+    private boolean thereIsSomeoneOnMyLeft(Stream<PlayerState> players, PlayerState myState) {
         switch (myState.direction) {
             case "N":
                 return thereIsSomebodyOnMyWay(players, myState.withDirection("W"));
